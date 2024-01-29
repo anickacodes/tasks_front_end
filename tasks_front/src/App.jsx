bdbcd90a2bacfeeb0e097badce14124f01b3da4a
anickacodes <anickalewis@pursuit.org>
2024-01-29 11:06:29 -0500

    dependecy install + pages

--- tasks_front/src/App.jsx
+++ tasks_front/src/App.jsx
@@ -1,19 +1,27 @@
 
 import axios from 'axios';
-import '../App.css'
+import './App.css'
 import NavBar from './components/NavBar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
 
 const App = () => {
 const url = import.meta.env.VITE_URL
 axios
-  .get(url)
+  .get(`${url}/users`)
   .then(res => console.log())
   .catch(err => console.error(err));
 
   return(
-    <>
+    <div>
     <NavBar />
-    </>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+    </Routes>
+</div>
   )
 }
 
+export default App
